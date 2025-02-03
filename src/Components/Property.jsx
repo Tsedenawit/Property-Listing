eab1fecd8040edd60c90d0dafb60c6ddfc090e8b
Tsedenawit <tsedenawit3@gmail.com>
2025-02-03 16:34:38 -0500

    sent

---
+++ src/Components/Property.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Property(){
+    return(
+        <div>
+            <h1 className="text-blue-300">
+                Hello red
+            </h1>
+        </div>
+    )
+}
