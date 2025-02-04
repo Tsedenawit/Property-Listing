551c59ca6e9a242962454337c1707888fa6a268d
Tsedenawit <tsedenawit3@gmail.com>
2025-02-04 14:23:31 -0500

    sent

--- src/Components/Property.jsx
+++ src/Components/Property.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Property(){
+    const [List, setList]= useState([]);
+
+    useEffect(()=>{
+        fetch( "https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/4-frontend-libaries/challenges/group_1/data/property-listing-data.json")
+      .then(response => response.json())
+      .then(data => {
+        setList(data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+      } ,[])
+console.log(List)
     return(
         <div>
             <h1 className="text-blue-300">
